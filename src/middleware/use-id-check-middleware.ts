fec3949a54817dd267ebbc5285e5fe084edd465d
artuur <arturcastrodossantos.com@gmail.com>
2024-07-03 07:59:33 -0300

    atualizado middlewares

--- src/middleware/use-id-check-middleware.ts
+++ src/middleware/use-id-check-middleware.ts
@@ -1,11 +1,15 @@
-import {NestMiddleware } from "@nestjs/common";
-import { NextFunction } from "express";
+import {BadRequestException, ExecutionContext, NestMiddleware } from "@nestjs/common";
+import { NextFunction, Request } from "express";
 
 
 export class UseIdCheckMiddleware implements NestMiddleware {
 
     use(req: Request, res: Response, next: NextFunction) {
+        
 
+        if (isNaN(Number(req.params.id)) || Number(req.params.id) >=0) {
+            throw new BadRequestException('ID inválido')
+        }
 
         next()
     }
