941b8ae4a9ad26addd5886acf61961eb01190a9e
artuur <arturcastrodossantos.com@gmail.com>
2024-09-14 00:06:08 -0300

    user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,8 +3,6 @@ import { CreateUserDTO } from "src/dto/create-user-dto";
 import { UpdateUserDTO } from "src/dto/update-user-dto";
 import { PrismaService } from "src/Prisma/prisma.Service";
 
-
-
 @Injectable()
 export class UserService {
 
@@ -12,142 +10,123 @@ export class UserService {
         
     }
 
-
     async create(data: CreateUserDTO) {
 
-        const {email, CPF} = data
+        const { email, CPF } = data;
 
         try {
 
             const userEmail = await this.prisma.user.count({
-
                 where: {
                     email: email
                 }
-            })
+            });
 
             const userCPF = await this.prisma.user.count({
-
                 where: {
                     CPF: CPF
                 }
-            })
+            });
 
             if (userEmail) {
-
-                return {message: 'Esse e-mail já existe'}
-
+                return { message: 'Esse e-mail já existe' };
             }
 
             if (userCPF) {
-
-                return {message: 'CPF incorreto'}
-
+                return { message: 'CPF incorreto' };
             }
 
             const user = await this.prisma.user.create({
-                
                 data
-            })
+            });
 
-            return user
+            return user;
 
-        } catch(error) {
-
-            console.log('não foi possivel criar usuario', error)
-            throw new BadRequestException('não foi possivel criar usuário')
-
+        } catch (error) {
+            console.log('não foi possivel criar usuario', error);
+            throw new BadRequestException('não foi possivel criar usuário');
         }
 
     }
 
-
     async read() {
-
-        return this.prisma.user.findMany()
-
+        return this.prisma.user.findMany();
     }
 
     async readOne(id: number) {
 
-        
-    try {
-            
-        const user = await this.prisma.user.findUnique({
-            where: {
-                id
+        try {
+
+            const user = await this.prisma.user.findUnique({
+                where: {
+                    id
+                }
+            });
+
+            if (!user) {
+                return { message: 'usuario não encontrado' };
             }
-        })
 
-        return {message: 'usuario encontrado', user}
+            return user;
 
-        } catch(error) {
-
-            console.log(error)
-            throw new NotFoundException('erro ao buscar')
+        } catch (error) {
+            console.log(error);
+            throw new NotFoundException('erro ao buscar');
         }
-    
-    
-    } 
+
+    }
 
     async update(data: UpdateUserDTO, id: number) {
 
-        
         try {
-            await this.verification(id)
+            await this.verification(id);
 
-        const user = await this.prisma.user.update({
+            const user = await this.prisma.user.update({
+                data,
+                where: {
+                    id
+                }
+            });
 
-            data, 
-            where: {
-                id
-            }
-            
-        })
+            return { message: 'usuário atualizado', user };
 
-        return {message: 'usuário atualizado', user}
-
-        } catch(error) {
-
-            console.log(error)
-            throw new BadRequestException('erro ao atualizar as informaçoes do usuario')
+        } catch (error) {
+            console.log(error);
+            throw new BadRequestException('erro ao atualizar as informaçoes do usuario');
         }
-        
-        
-        
+
     }
 
     async delete(id: number) {
 
-
         try {
-            await this.verification(id)
+            await this.verification(id);
 
-        return this.prisma.user.delete({
-            where: {
-                id
-            }
-        })
+            return this.prisma.user.delete({
+                where: {
+                    id
+                }
+            });
+
         } catch (error) {
-            throw new BadRequestException('erro ao deletar') 
-
+            throw new BadRequestException('erro ao deletar');
         }
+
     }
 
-    
     async verification(id: number) {
 
         const user = await this.prisma.user.count({
-            where:{
-                id:id
-             }
-        })
+            where: {
+                id: id
+            }
+        });
 
         if (!user) {
-            throw new BadRequestException('id inválido')
+            throw new BadRequestException('id inválido');
         }
 
-        return user
+        return user;
 
     }
 
